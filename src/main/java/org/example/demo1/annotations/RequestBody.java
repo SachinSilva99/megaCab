3a72723bc66bbe4b2876eefb13da5bdfe25d13a2
sachin_s <sachin_s@epiclanka.net>
2025-02-26 11:24:05 +0530

    drivers api completed

---
+++ src/main/java/org/example/demo1/annotations/RequestBody.java
@@ -0,0 +1,14 @@
+package org.example.demo1.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Author : SachinSilva
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+public @interface RequestBody {
+}
